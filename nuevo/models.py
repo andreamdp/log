dcf6974c56051a3cac9e0cda484534e64dffa25f
Andrea <andremdq@gmail.com>
2017-09-12 15:23:54 -0300

    Registro la clase Log

--- nuevo/models.py
+++ nuevo/models.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.db import models
+class log(models.Model):
+    fecha = models.DateField(auto_now=True)
+    tipo=models.CharField(max_length=50,'Tipo de Rebote')
+    email=models.CharField(max_length=70)
+    
 
-# Create your models here.
